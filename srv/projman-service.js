ddaa2fe1e5af88a5a1b76a25dc8f8c71f467353c
Peng Ward <ward.peng@gmail.com>
2022-07-22 05:18:52 +0000

    Added creditienals

--- srv/projman-service.js
+++ srv/projman-service.js
@@ -1,4 +1,14 @@
 const cds = require('@sap/cds');
+const {
+    readSFSF_User,
+    createEmployee,
+    updateEmployee,
+    createItem,
+    deleteChildren,
+    deleteUnassignedEmployees,
+    beforeSaveProject,
+    afterSaveProject
+} = require('./lib/handlers');
 
 module.exports = cds.service.impl(async function () {
     /*** SERVICE ENTITIES ***/
@@ -8,14 +18,21 @@ module.exports = cds.service.impl(async function () {
         SFSF_User,
     } = this.entities;
 
-    const {
-        readSFSF_User
-    } = require('./lib/handlers');
-
     /*** HANDLERS REGISTRATION ***/
     // ON events
     this.on('READ', SFSF_User, readSFSF_User);
+
     // BEFORE events
+    this.before('CREATE', Member, createEmployee);
+    this.before('UPDATE', Member, updateEmployee);
+    this.before('DELETE', Project, deleteChildren);
+    this.before('DELETE', Member, deleteChildren);
+    this.before('SAVE', Project, beforeSaveProject); // Fiori Draft support
 
     // AFTER events
+    this.after('CREATE', Member, createItem);
+    this.after('UPDATE', Member, deleteUnassignedEmployees);
+    this.after('DELETE', Project, deleteUnassignedEmployees);
+    this.after('DELETE', Member, deleteUnassignedEmployees);
+    this.after('SAVE', Project, afterSaveProject); // Fiori Draft support
 });
